76f5c5184d4ea1e195dc5799f96b9bdaa0edf307
seculiraul <71821279+seculiraul@users.noreply.github.com>
2021-02-25 19:16:53 +0200

    CurrencyConverter

--- MainActivity.java
+++ MainActivity.java
@@ -1,121 +1,29 @@
-package com.myappcompany.rob.connect3game;
+package com.myappcompany.rob.currencyconverter;
 
-import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.GridLayout;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    // 0: yellow, 1: red, 2: empty
+    public void convertCurrency(View view) {
 
-    int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
+        Log.i("Info", "Button pressed");
 
-    int[][] winningPositions = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
+        EditText editText = (EditText) findViewById(R.id.editText);
 
-    int activePlayer = 0;
+        String amountInPounds = editText.getText().toString();
 
-    boolean gameActive = true;
+        double amountInPoundsDouble = Double.parseDouble(amountInPounds);
 
-    public void dropIn(View view) {
+        double amountInDollarsDouble = amountInPoundsDouble * 1.3;
 
-        ImageView counter = (ImageView) view;
+        String amountInDollarsString = String.format("%.2f", amountInDollarsDouble);
 
-        int tappedCounter = Integer.parseInt(counter.getTag().toString());
-
-        if (gameState[tappedCounter] == 2 && gameActive) {
-
-            gameState[tappedCounter] = activePlayer;
-
-            counter.setTranslationY(-1500);
-
-            if (activePlayer == 0) {
-
-                counter.setImageResource(R.drawable.yellow);
-
-                activePlayer = 1;
-
-            } else {
-
-                counter.setImageResource(R.drawable.red);
-
-                activePlayer = 0;
-
-            }
-
-            counter.animate().translationYBy(1500).rotation(3600).setDuration(300);
-
-            for (int[] winningPosition : winningPositions) {
-
-                if (gameState[winningPosition[0]] == gameState[winningPosition[1]] && gameState[winningPosition[1]] == gameState[winningPosition[2]] && gameState[winningPosition[0]] != 2) {
-
-                    // Somone has won!
-
-                    gameActive = false;
-
-                    String winner = "";
-
-                    if (activePlayer == 1) {
-
-                        winner = "Yellow";
-
-                    } else {
-
-                        winner = "Red";
-
-                    }
-
-                    Button playAgainButton = (Button) findViewById(R.id.playAgainButton);
-
-                    TextView winnerTextView = (TextView) findViewById(R.id.winnerTextView);
-
-                    winnerTextView.setText(winner + " has won!");
-
-                    playAgainButton.setVisibility(View.VISIBLE);
-
-                    winnerTextView.setVisibility(View.VISIBLE);
-
-                }
-
-            }
-        }
-    }
-
-    public void playAgain(View view) {
-
-        Button playAgainButton = (Button) findViewById(R.id.playAgainButton);
-
-        TextView winnerTextView = (TextView) findViewById(R.id.winnerTextView);
-
-        playAgainButton.setVisibility(View.INVISIBLE);
-
-        winnerTextView.setVisibility(View.INVISIBLE);
-
-        GridLayout gridLayout = (GridLayout) findViewById(R.id.gridLayout);
-
-        for(int i=0; i<gridLayout.getChildCount(); i++) {
-
-            ImageView counter = (ImageView) gridLayout.getChildAt(i);
-
-            counter.setImageDrawable(null);
-
-        }
-
-        for (int i=0; i<gameState.length; i++) {
-
-            gameState[i] = 2;
-
-        }
-
-        activePlayer = 0;
-
-        gameActive = true;
+        Toast.makeText(this, "£" + amountInPounds + " is $" + amountInDollarsString, Toast.LENGTH_LONG).show();
 
     }
 
